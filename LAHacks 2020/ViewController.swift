532450a1e737c23c088e11822df1a17372c8470c
Matthew Krager <matthewkrager@gmail.com>
2020-03-26 16:33:27 -0700

    Initial Commit

---
+++ LAHacks 2020/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LAHacks 2020
+//
+//  Created by Matthew Krager on 3/26/20.
+//  Copyright © 2020 Matthew Krager. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
